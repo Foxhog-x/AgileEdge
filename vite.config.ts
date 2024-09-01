cea6ecfe3e900705bf385f820ea47b54fb7f5d62
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-09-01 23:35:34 +0530

    production ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for the build
+    sourcemap: false, // Optional: Generate sourcemaps for production
+  },
 });
